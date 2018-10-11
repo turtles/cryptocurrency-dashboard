21bdfca499790ca05870a9dff79f04e5d0280138
Leah Sandacz <sandas@outlook.com>
2018-10-10 18:09:46 -0700

    Remove console log

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ class App extends Component {
     const { onFetchCryptocurrencies } = this.props;
     onFetchCryptocurrencies();
     setInterval(onFetchCryptocurrencies, 60 * 1000);
-    console.log('App will mount');
   }
   render() {
     return (
