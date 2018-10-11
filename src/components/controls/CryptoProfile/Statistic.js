c16961a3274a1f000907eb017d05ea40f09da911
Leah Sandacz <sandas@outlook.com>
2018-10-10 20:52:13 -0700

    Hide statistic if not present in data

--- src/components/controls/CryptoProfile/Statistic.js
+++ src/components/controls/CryptoProfile/Statistic.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Statistic = ({name, children}) => (
     <div>
         <h4>{name}</h4>
-        <p>{children}</p>
+        <p>{
+            children ? children : 'Not available'
+        }</p>
     </div>
 )
 
