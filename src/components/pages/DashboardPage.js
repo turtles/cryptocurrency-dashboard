fddb07175650af71ea1aa7d31806801eb5325d22
Leah Sandacz <sandas@outlook.com>
2018-10-10 17:46:16 -0700

    Load data on a 60 second interval and show spinner when loading

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchCryptocurrencies } from '../../redux/actions/cryptocurrenciesActions';
+
 import CryptoTable from '../controls/CryptoTable';
 import Header from '../controls/Header';
 
-const DashboardPage = () => (
-    <React.Fragment>
-        <Header />
-        <CryptoTable />
-    </React.Fragment>
-);
+const mapStateToProps = ({loading, data}) => ({
+    loading,
+    data
+})
 
-export default DashboardPage;
+const mapDispatchToProps = dispatch => {
+	return {
+        onFetchCryptocurrencies:
+            ()=>dispatch(fetchCryptocurrencies())
+    }
+}
+
+class DashboardPage extends React.Component {
+    componentWillMount() {
+        const { onFetchCryptocurrencies } = this.props;
+        onFetchCryptocurrencies();
+        setInterval(onFetchCryptocurrencies, 60 * 1000);
+    }
+    render() {
+        const { loading, data } = this.props;
+
+        return (
+            <React.Fragment>
+                <Header />
+                <CryptoTable loading={loading} data={data} />
+            </React.Fragment>
+        );
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardPage);
